6716bc79c889f700f80ecbae9b4924f58d561f55
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-15 22:27:19 +0530

    report  Design

--- src/Components/Outlets/Report.jsx
+++ src/Components/Outlets/Report.jsx
@@ -1,9 +1,39 @@
-
+import Table from "react-bootstrap/Table";
 
 const Report = () => {
   return (
-    <div>Report</div>
-  )
-}
+    <div>
+      <Table striped bordered hover variant="dark" >
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Income</th>
+            <th>OutGoing</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
 
-export default Report
+export default Report;
