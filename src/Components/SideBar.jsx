a7a1812560c821dd6ffb869b1d88e36ec13b23a9
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-15 21:34:12 +0530

    FIles Created

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+const SideBar = () => {
+  return (
+    <>
+      <ul>
+        <Link to="/">
+          {" "}
+          <li>Dashboard</li>
+        </Link>
+        <Link to="/report">
+          {" "}
+          <li>Report</li>
+        </Link>
+        <Link to="/Accounts">
+          <li>Accounts</li>
+        </Link>
+        <Link to="/Members">
+          {" "}
+          <li>Members</li>
+        </Link>
+        <Link to="/Transaction">
+          {" "}
+          <li>RepTransactionort</li>
+        </Link>
+        <Link to="/Settings">
+          {" "}
+          <li>Settings</li>
+        </Link>
+      </ul>
+    </>
+  );
+};
+
+export default SideBar;
