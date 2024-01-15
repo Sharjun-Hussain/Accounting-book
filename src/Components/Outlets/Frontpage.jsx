176bc7e7e995b29d99f01c497bdeeca17fed513d
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-15 22:23:51 +0530

    Frontpage Design

--- src/Components/Outlets/Frontpage.jsx
+++ src/Components/Outlets/Frontpage.jsx
@@ -1,9 +1,62 @@
-
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 const Frontpage = () => {
   return (
-    <div>Frontpage</div>
-  )
-}
+    <>
+      <Container>
+        <Row>
+          <Col md={4} xs={12} xl={4}>
+            <Card>
+              <Card.Body>Rs . 10,000</Card.Body>
+              <Card.Title>Kaiyiruppu</Card.Title>
+            </Card>
+          </Col>
 
-export default Frontpage
+          <Col md={4} xs={12} xl={4}>
+            <Card>
+              <Card.Body>Rs . 10,000</Card.Body>
+              <Card.Title>Vangi iruppu</Card.Title>
+            </Card>
+          </Col>
+
+          <Col md={4} xs={12} xl={4}>
+            <Card>
+              <Card.Body>Rs . 10,000</Card.Body>
+              <Card.Title>Kaiyiruppu</Card.Title>
+            </Card>
+          </Col>
+
+          <Col md={4} xs={12} xl={4}>
+            <Card>
+              <Card.Body>Rs . 10,000</Card.Body>
+              <Card.Title>Kaiyiruppu</Card.Title>
+            </Card>
+          </Col>
+
+          <Col md={4} xs={12} xl={4}>
+            <Card>
+              <Card.Body>Rs . 10,000</Card.Body>
+              <Card.Title>Kaiyiruppu</Card.Title>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <Transaction />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Frontpage;
+
+// Recent Transcation
+
+
+const Transaction = () => {
+  return <>Hello world</>;
+};
