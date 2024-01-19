07d8579154c4edee2b148225659901da532911d3
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-01-19 18:16:40 +0530

    Transaction Table Designed

--- src/Components/Tables/TransactionTable.jsx
+++ src/Components/Tables/TransactionTable.jsx
@@ -1,11 +1,11 @@
 import { useState, useRef } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Form } from "react-bootstrap";
 // import CheckBox from "../Utilities/CheckBox";
 
 const TransactionTable = () => {
   //CheckBox All Configuration Start
   const [selected, setSelected] = useState([]);
-  const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState(false);
   const listOptions = [
     "apple",
     "banana",
@@ -16,67 +16,64 @@ const TransactionTable = () => {
     "honeydew melon",
   ];
 
-// const HandleCheck = (event) =>{
+  // const HandleCheck = (event) =>{
 
-//     const {value, checked} = event.target;
+  //     const {value, checked} = event.target;
 
-//     if(checked){
-//         setSelected(e =>[...e,value])
-        
+  //     if(checked){
+  //         setSelected(e =>[...e,value])
 
-//     }else {
-//         setSelected(e => {
-//             return [...e.filter(ev => ev !== value)]
-//         })
-        
-//     }
-    
+  //     }else {
+  //         setSelected(e => {
+  //             return [...e.filter(ev => ev !== value)]
+  //         })
 
-// }
+  //     }
 
+  // }
 
-console.log(selected)
+  console.log(selected);
 
-const rowCheckbox = useRef('')
-const toggleCheckboxes = () => {
-    setChecked(prevState => !prevState)
+  const rowCheckbox = useRef("");
+  const toggleCheckboxes = () => {
+    setChecked((prevState) => !prevState);
 
-    if(checked){
-        setSelected(listOptions);
-    } else{
-        setSelected([]);
+    if (checked) {
+      setSelected(listOptions);
+    } else {
+      setSelected([]);
     }
-}
+  };
 
-// const handleSelectAll = (value) => {
-//     if (value) {
-//       setSelected(listOptions);
-//     } else {
-//       setSelected([]);
-//     }
-//   };
+  // const handleSelectAll = (value) => {
+  //     if (value) {
+  //       setSelected(listOptions);
+  //     } else {
+  //       setSelected([]);
+  //     }
+  //   };
 
-//   function handleSelect(value, name) {
-//     if (value) {
-//       setSelected([...selected, name]);
-//       console.log(selected)
-//     } else {
-//       setSelected(selected.filter((item) => item !== name));
-//       console.log(selected)
-//     }
-//   }
+  //   function handleSelect(value, name) {
+  //     if (value) {
+  //       setSelected([...selected, name]);
+  //       console.log(selected)
+  //     } else {
+  //       setSelected(selected.filter((item) => item !== name));
+  //       console.log(selected)
+  //     }
+  //   }
 
-//   function selectAll(value) {
-//     if (value) {
-//       // if true
-//       setSelected(listOptions); 
-//       console.log(selected)
-//     } else {
-//       // if false
-//       setSelected([]); 
-//       console.log(selected)
-//     }
-//   }
+  //   function selectAll(value) {
+  //     if (value) {
+  //       // if true
+  //       setSelected(listOptions);
+  //       console.log(selected)
+  //     } else {
+  //       // if false
+  //       setSelected([]);
+  //       console.log(selected)
+  //     }
+  //   }
 
   //CheckBox Configuration Stop
 
@@ -85,28 +82,48 @@ const toggleCheckboxes = () => {
       <thead>
         <tr>
           <th>
-          <input
-          type="checkbox"
-          onClick={toggleCheckboxes}
-          
-        />
+            <Form.Check
+              className="text-center"
+              type="checkbox"
+              onClick={toggleCheckboxes}
+            />
+            {/* <input type="checkbox" onClick={toggleCheckboxes} /> */}
           </th>
 
           <th>#</th>
-          <th className="text-center"> Memebers</th>
+          <th className="text-center"> Date</th>
+          <th className="text-center"> Description</th>
+          <th className="text-center"> From</th>
+          <th className="text-center"> to</th>
+          <th className="text-center"> Amount</th>
+          <th className="text-center"> Documents</th>
           <th className="text-center">Actions</th>
         </tr>
       </thead>
       <tbody>
-        {listOptions.map((item, key) => {
-          return (
-            <div key={key}>
-               <label className='mb-1'> <input type="checkbox" ref={rowCheckbox} id="amenities1" className='' name={item} value={item} checked={checked} ></input>{item}</label>
-              
-              
-            </div>
-          );
-        })}
+        <tr>
+          <td>
+            <Form.Check
+              className="text-center"
+              type="checkbox"
+              ref={rowCheckbox}
+              name="" // name={item}  format
+              value="" // value={item}  format
+              checked={checked}
+            />
+          </td>
+          <td>1</td>
+          <td>Buy Tea</td>
+          <td></td>
+        </tr>
+        {/* <input
+          type="checkbox"
+          ref={rowCheckbox}
+          className=""
+          name="" // name={item}  format
+          value="" // value={item}  format
+          checked={checked}
+        ></input> */}
       </tbody>
     </Table>
   );
