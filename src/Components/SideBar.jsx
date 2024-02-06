42fbf9182ca9325a3033ee5a27f8e754a89a1e08
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-02-06 21:59:31 +0530

    Some UI Changes

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,36 +1,40 @@
-import { Link } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
+import report from '../assets/Icons/report.svg'
+
+
+
 
 const SideBar = () => {
   return (
     <>
       <ul>
-        <Link to="/">
+        <NavLink to="/" className="d-flex ps-3 ps-3">
           {" "}
-          <li>Dashboard</li>
-        </Link>
-        <Link to="report">
+          <img src={report}  width={30}/> <li>Dashboard</li>
+        </NavLink>
+        <NavLink to="report" className="d-flex ps-3">
           {" "}
-          <li>Report</li>
-        </Link>
-        <Link to="/Accounts">
-          <li>Accounts</li>
-        </Link>
-        <Link to="/Members">
+          <img src={report}  width={30}/><li>Report</li>
+        </NavLink>
+        <NavLink to="/Accounts" className="d-flex ps-3">
+        <img src={report}  width={30}/> <li>Accounts</li>
+        </NavLink>
+        <NavLink to="/Members" className="d-flex ps-3">
           {" "}
-          <li>Members</li>
-        </Link>
-        <Link to="/Transaction">
+          <img src={report}  width={30}/>  <li>Members</li>
+        </NavLink>
+        <NavLink to="/Transaction" className="d-flex ps-3">
           {" "}
-          <li>Transaction</li>
-        </Link>
-        <Link to="/Sandha">
+          <img src={report}  width={30}/>  <li>Transaction</li>
+        </NavLink>
+        <NavLink to="/Sandha" className="d-flex ps-3">
           {" "}
-          <li>Sandha</li>
-        </Link>
-        <Link to="/Settings">
+          <img src={report}  width={30}/>  <li>Sandha</li>
+        </NavLink>
+        <NavLink to="/Settings" className="d-flex ps-3">
           {" "}
-          <li>Settings</li>
-        </Link>
+          <img src={report}  width={30}/>  <li>Settings</li>
+        </NavLink>
       </ul>
     </>
   );
