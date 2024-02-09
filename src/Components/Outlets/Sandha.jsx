da42543fa86d9a2a10173699f16b27a01377112f
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-02-10 00:32:25 +0530

    Sandha Card Aligned

--- src/Components/Outlets/Sandha.jsx
+++ src/Components/Outlets/Sandha.jsx
@@ -4,113 +4,144 @@ import { Link, Outlet } from "react-router-dom";
 import SandhaAddModal from "../Modals/SandhaAdd";
 import { useState } from "react";
 
-
 const SandhaMainPage = () => {
   const today = new Date();
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   const ThisMonth = months[today.getMonth()];
-  const PreviousMonth = months[today.getMonth()-1];
+  const PreviousMonth = months[today.getMonth() - 1];
   const [ModalShow, setModalShow] = useState(false);
   return (
     <>
       <Container fluid>
         <>
           <div className="Front-cards-Background-card  mt-3 ">
-            <Col className="d-flex"> 
+            <Col className="d-flex">
               <h3 className="text-white">Sandha Details</h3>
 
               <div className="ms-auto">
-                <Button onClick={() => setModalShow(true)}>
-                  Add Sandha
-                </Button>
+                <Button onClick={() => setModalShow(true)}>Add Sandha</Button>
                 <SandhaAddModal
                   show={ModalShow}
-                  onHide={() => setModalShow(false)} />
+                  onHide={() => setModalShow(false)}
+                />
               </div>
             </Col>
           </div>
         </>
         <>
           <div className="d-flex flex-wrap  mt-3 Front-cards-Background-card   ">
-            <Col md={4} xs={12} xl={3} className="">
+            <Col md={6} xs={12} lg={4} xl={3} className="">
               <Link to="this-month">
-                <Card className="d-flex flex-column mx-1 my-2">
-                  <Card.Body className="d-flex flex-row justify-content-between  ">
+                <Card className="d-flex flex-column me-md-1 my-2">
+                  <Card.Body className="d-flex flex-row justify-content-between">
                     <div>
                       {" "}
                       <h2>Rs . 10,000</h2>
-                      <Card.Title> இம்மாத  சந்தா </Card.Title>
+                      <Card.Title>இம்மாத சந்தா </Card.Title>
                     </div>
                     <span>Icon</span>
                   </Card.Body>
-                </Card>
+                </Card>{" "}
               </Link>
             </Col>
 
-            <Col md={4} xs={12} xl={3} className="">
-              <Card className="d-flex flex-column mx-1 my-2">
-                <Card.Body className="d-flex flex-row justify-content-between  ">
-                  <div>
-                    {" "}
-                    <h2>100</h2>
-                    <Card.Title>சந்தா கொடுத்தவர்கள் </Card.Title>
-                  </div>
-                  <span>Icon</span>
-                </Card.Body>
-              </Card>
+            <Col md={6} xs={12} lg={4} xl={3} className="">
+              <Link to="this-month">
+                <Card className="d-flex flex-column me-md-1 my-2">
+                  <Card.Body className="d-flex flex-row justify-content-between">
+                    <div>
+                      {" "}
+                      <h2>Rs . 10,000</h2>
+                      <Card.Title>இம்மாத சந்தா </Card.Title>
+                    </div>
+                    <span>Icon</span>
+                  </Card.Body>
+                </Card>{" "}
+              </Link>
             </Col>
 
-            <Col md={4} xs={12} xl={3} className="">
-              <Card className="d-flex flex-column mx-1 my-2">
-                <Card.Body className="d-flex flex-row justify-content-between  ">
-                  <div>
-                    {" "}
-                    <h2>50</h2>
-                    <Card.Title>சந்தா கொடுக்காதவர்கள்</Card.Title>
-                  </div>
-                  <span>Icon</span>
-                </Card.Body>
-              </Card>
+            <Col md={6} xs={12} lg={4} xl={3} className="">
+              <Link to="this-month">
+                <Card className="d-flex flex-column me-md-1 my-2">
+                  <Card.Body className="d-flex flex-row justify-content-between">
+                    <div>
+                      {" "}
+                      <h2>Rs . 10,000</h2>
+                      <Card.Title>இம்மாத சந்தா </Card.Title>
+                    </div>
+                    <span>Icon</span>
+                  </Card.Body>
+                </Card>{" "}
+              </Link>
             </Col>
 
-            <Col md={4} xs={12} xl={3} className="">
-              <Card className="d-flex flex-column mx-1 my-2">
-                <Card.Body className="d-flex flex-row justify-content-between  ">
-                  <div>
-                    {" "}
-                    <h2>Rs . 10,000</h2>
-                    <Card.Title>வர வேண்டிய சந்தா </Card.Title>
-                  </div>
-                  <span>Icon</span>
-                </Card.Body>
-              </Card>
+            <Col md={6} xs={12} lg={4} xl={3} className="">
+              <Link to="this-month">
+                <Card className="d-flex flex-column me-md-1 my-2">
+                  <Card.Body className="d-flex flex-row justify-content-between">
+                    <div>
+                      {" "}
+                      <h2>Rs . 10,000</h2>
+                      <Card.Title>இம்மாத சந்தா </Card.Title>
+                    </div>
+                    <span>Icon</span>
+                  </Card.Body>
+                </Card>{" "}
+              </Link>
             </Col>
 
-            <Col md={4} xs={12} xl={3} className="">
-              <Card className="d-flex flex-column mx-1 my-2">
-                <Card.Body className="d-flex flex-row justify-content-between  ">
-                  <div>
-                    {" "}
-                    <h2>Rs . 10,000</h2>
-                    <Card.Title>சென்ற மாத சந்தா - {PreviousMonth}</Card.Title>
-                  </div>
-                  <span>Icon</span>
-                </Card.Body>
-              </Card>
+            <Col md={6} xs={12} lg={4} xl={3} className="">
+              <Link to="this-month">
+                <Card className="d-flex flex-column me-md-1 my-2">
+                  <Card.Body className="d-flex flex-row justify-content-between">
+                    <div>
+                      {" "}
+                      <h2>Rs . 10,000</h2>
+                      <Card.Title>இம்மாத சந்தா </Card.Title>
+                    </div>
+                    <span>Icon</span>
+                  </Card.Body>
+                </Card>{" "}
+              </Link>
             </Col>
 
-            <Col md={4} xs={12} xl={3} className="">
-              <Card className="d-flex flex-column mx-1 my-2">
-                <Card.Body className="d-flex flex-row justify-content-between  ">
-                  <div>
-                    {" "}
-                    <h2>Rs . 10,000</h2>
-                    <Card.Title>{PreviousMonth} Due Sandha</Card.Title>
-                  </div>
-                  <span>Icon</span>
-                </Card.Body>
-              </Card>
+            <Col md={6} xs={12} lg={4} xl={3} className="">
+              <Link to="this-month">
+                <Card className="d-flex flex-column me-md-1 my-2">
+                  <Card.Body className="d-flex flex-row justify-content-between">
+                    <div>
+                      {" "}
+                      <h2>Rs . 10,000</h2>
+                      <Card.Title>இம்மாத சந்தா </Card.Title>
+                    </div>
+                    <span>Icon</span>
+                  </Card.Body>
+                </Card>{" "}
+              </Link>
             </Col>
+
+            
+
+           
+
+            
+
+            
+
+            
           </div>
         </>
       </Container>
