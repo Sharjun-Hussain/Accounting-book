a4a5078ac3d15c7a7562b8062a326346261f1885
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-02-10 23:18:36 +0530

    Month Select Added and Multiple Selection

--- src/Components/Modals/SandhaAdd.jsx
+++ src/Components/Modals/SandhaAdd.jsx
@@ -1,8 +1,44 @@
-import { Col, Container, Row, Form } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import axios from "axios";
+import { useState } from "react";
+import {
+  Col,
+  Container,
+  Row,
+  Form,
+  Dropdown,
+  Button,
+  Modal,
+} from "react-bootstrap";
 
 const SandhaAddModal = (props) => {
+  const [SelectedMonths, SetSelectedMonths] = useState([]);
+  const Months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+
+
+  const toggleLang = (option) => {
+    if (SelectedMonths.includes(option)) {
+      SetSelectedMonths(SelectedMonths.filter((item) => item !== option));
+    } else {
+      SetSelectedMonths([...SelectedMonths, option]);
+    }
+  };
+
+  
+
   return (
     <Container>
       <Row>
@@ -21,30 +57,49 @@ const SandhaAddModal = (props) => {
             <Modal.Body>
               <Form>
                 <Row className="mb-3">
-                  <Form.Group as={Col} controlId="formGridName">
+                  <Form.Group as={Col} controlId="MemberName">
                     <Form.Label>Member Name</Form.Label>
                     <Form.Control type="text" placeholder="Sharjun-Hussain" />
                   </Form.Group>
+                  <Form.Group as={Col} controlId="Months">
+                    <Dropdown style={{marginTop:"31px",marginLeft:"31px"}}>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Select Months
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu>
+                        {Months.map((option, index) => (
+                          <Dropdown.Item
+                            key={index}
+                            onClick={() => toggleLang(option)}
+                            active={SelectedMonths.includes(option)}
+                          >
+                            {option}
+                          </Dropdown.Item>
+                        ))}
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Form.Group>
+                  
+                  
                 </Row>
 
                 <Row>
-                  
-                  <Form.Group
-                    as={Col}
-                    className="mb-3"
-                    controlId="formGridPhone"
-                  >
+                  <Form.Group as={Col} className="mb-3" controlId="Amount">
                     <Form.Label>Sandha Amount</Form.Label>
                     <Form.Control type="number" placeholder="" />
                   </Form.Group>
 
                   <Form.Group
                     as={Col}
-                    className="mb-3"
+                    className="my-1"
                     controlId="formGridPhone"
                   >
-                    <Form.Label>Status</Form.Label>
-                    
+                    {SelectedMonths.map((btn)=>{
+                    return(
+                      <Button className="mx-1 my-1" key={btn}>{btn}</Button>
+                    )
+                  })}
+                 
                   </Form.Group>
                 </Row>
 
