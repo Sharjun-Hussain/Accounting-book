b3381806c5718d6221de432e8efb0151beaf5928
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-02-17 22:55:09 +0530

    Snadha Route Created

--- BackEnd/Routes/Sandha.js
+++ BackEnd/Routes/Sandha.js
@@ -1,2 +1,12 @@
-const SandhaModel = require('../Models/Sandha');
+const { FetchAllSandha, AddSandha, DeleteSandha, UpdateSandha } = require('../Controllers/Sandha');
 
+
+const express = require('express');
+const app =express.Router();
+
+app.route("/All").get(FetchAllSandha);
+app.route("/Add").post(AddSandha);
+app.route("/Delete/:id").post(DeleteSandha);
+app.route("/Update/:id").post(UpdateSandha);
+
+module.exports = app
