b13178fbdedf790b91233e3dc532efba7ddef1c0
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-02-26 16:20:14 +0530

    Donation Route Configured

--- BackEnd/Routes/Donation.js
+++ BackEnd/Routes/Donation.js
@@ -1,12 +1,12 @@
-const { FetchAllDonation, AddDonation, DeleteDonation, UpdateDonation } = require('../Controllers/Donation');
+const { FetchAllDonation, AddDonation, DeleteDonation, UpdateDonation, FetchSpecicMonthDonationDetails, FetchSpecicMonthDonationSum } = require('../Controllers/Donation');
 
 
 const express = require('express');
 const app =express.Router();
 
 app.route("/All").get(FetchAllDonation);
-app.route("/Month/:Month").get(FetchSpecicMonthSandhaDetails);
-app.route("/Month/:Month/Sum").get(FetchSpecicMonthSandhaSum);
+app.route("/Month/:Month").get(FetchSpecicMonthDonationDetails);
+app.route("/Month/:Month/Sum").get(FetchSpecicMonthDonationSum);
 app.route("/Add").post(AddDonation);
 app.route("/Delete/:id").post(DeleteDonation);
 app.route("/Update/:id").post(UpdateDonation);
