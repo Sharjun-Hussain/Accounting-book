82c89208e5d0b32b57703dc73d555f1d235945f4
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-05-18 02:12:35 +0530

    UI Changes color correction and setting started

--- FrontEnd/src/Components/Outlets/Settings.jsx
+++ FrontEnd/src/Components/Outlets/Settings.jsx
@@ -1,7 +1,44 @@
-
-
+import { useState } from "react";
+import { Col, Container, Row, Form } from "react-bootstrap";
 export const Settings = () => {
+  const [Name, setName] = useState("");
   return (
-    <div>Settings</div>
-  )
-}
+    <Container>
+      <Row>
+        <Col>
+          <div className="text-white">
+            <h5>General Settings</h5>
+            <p style={{ marginTop: "-10px", color: "wheat" }}>
+              {" "}
+              Settings and options for your application{" "}
+            </p>
+            <hr />
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={2}>
+          <div className="text-white">
+            <p>Name :</p>
+          </div>
+        </Col>
+        <Col md={5}>
+          <div className="text-white">
+            <Form.Group as={Col} controlId="formGridName">
+              <Form.Control
+                type="text"
+                value={Name}
+                autoComplete="off"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="Sharjun-Hussain"
+              />
+            </Form.Group>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
