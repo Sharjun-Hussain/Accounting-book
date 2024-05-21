b162bef7baf91ac20e9a2f9f0b2fa33650408b7a
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-05-21 18:23:07 +0530

    Add user experience in Login

--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -1,6 +1,25 @@
 import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
 import hadhiLogo from "./assets/images/hadhi-logo.png";
+import { useState, useRef ,useEffect} from "react";
 const Login = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState(null);
+
+  const inputref = useRef(null);
+
+  useEffect(() => {
+  
+    inputref.current.focus();
+  
+    
+  }, [])
+  
+
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    console.log(Email, Password);
+  };
+
   return (
     <Container fluid className="login-Container">
       <Row>
@@ -9,27 +28,56 @@ const Login = () => {
             <div className="d-flex justify-content-center flex-column ">
               <Image className="login-logo" src={hadhiLogo} />
               <h2 className="text-center">Masjidhul Haadhi</h2>
-              <h4 style={{marginTop:"-13px", color:"#00728B"}} className="text-center "> Accounting Book</h4>
+              <h4
+                style={{ marginTop: "-13px", color: "#00728B" }}
+                className="text-center "
+              >
+                {" "}
+                Accounting Book
+              </h4>
             </div>
-            <hr  />
-            <Form.Group>
-              <Form.Label style={{marginBottom:"-5px" , color:"white"}}>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter Your Email Address"
-                className="form-control"
-              />
-            </Form.Group>
-            <Form.Group className="mt-3">
-              <Form.Label style={{marginBottom:"-5px" , color:"white"}}>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter The Password"
-                className="form-control"
-              />
-            </Form.Group>
+            <hr />
+            <Form>
+              <Form.Group>
+                <Form.Label style={{ marginBottom: "-5px", color: "white" }}>
+                  Email
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Your Email Address"
+                  className="form-control"
+                  name="Email"
+                  required
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  ref={inputref}
+                />
+              </Form.Group>
+              <Form.Group className="mt-3">
+                <Form.Label style={{ marginBottom: "-5px", color: "white" }}>
+                  Password
+                </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter The Password"
+                  className="form-control"
+                  name="Password"
+                  required
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </Form.Group>
 
-            <Button className="login-btn">Login</Button>
+              <Button
+                onClick={HandleSubmit}
+                type="submit"
+                className="login-btn"
+              >
+                Login
+              </Button>
+            </Form>
           </div>
         </Col>
       </Row>
