c8ab4aa22754a5bdff4a2314fc1aa3dc03ba7ee1
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-06-06 18:08:03 +0530

    sidebar Optimised

--- FrontEnd/src/Components/SideBar.jsx
+++ FrontEnd/src/Components/SideBar.jsx
@@ -1,99 +1,88 @@
 /* eslint-disable no-undef */
 import { NavLink } from "react-router-dom";
 import SpaceDashboardOutlinedIcon from "@mui/icons-material/SpaceDashboardOutlined";
-import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
-import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
-import SummarizeIcon from '@mui/icons-material/Summarize';
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
-import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
-import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
-import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-import PaidIcon from '@mui/icons-material/Paid';
-import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
-import VolunteerActivismOutlinedIcon from '@mui/icons-material/VolunteerActivismOutlined';
-import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import SummarizeOutlinedIcon from "@mui/icons-material/SummarizeOutlined";
+import SummarizeIcon from "@mui/icons-material/Summarize";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ReceiptLongOutlinedIcon from "@mui/icons-material/ReceiptLongOutlined";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
+import PaidIcon from "@mui/icons-material/Paid";
+import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import VolunteerActivismOutlinedIcon from "@mui/icons-material/VolunteerActivismOutlined";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+import SettingsIcon from "@mui/icons-material/Settings";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
-
+const item = [
+  {
+    name: "Dashboard",
+    icon: <SpaceDashboardOutlinedIcon />,
+    activeIcon: <SpaceDashboardIcon />,
+    path: "/",
+  },
+  {
+    name: "Report",
+    icon: <SummarizeOutlinedIcon />,
+    activeIcon: <SummarizeIcon />,
+    path: "/report",
+  },
+  {
+    name: "Accounts",
+    icon: <AccountBalanceWalletOutlinedIcon />,
+    activeIcon: <AccountBalanceWalletIcon />,
+    path: "/Accounts",
+  },
+  {
+    name: "Members",
+    icon: <PeopleOutlinedIcon />,
+    activeIcon: <PeopleAltIcon />,
+    path: "/Members",
+  },
+  {
+    name: "Transaction",
+    icon: <ReceiptLongOutlinedIcon />,
+    activeIcon: <ReceiptLongIcon />,
+    path: "/Transaction",
+  },
+  {
+    name: "Sandha",
+    icon: <PaidOutlinedIcon />,
+    activeIcon: <PaidIcon />,
+    path: "/Sandha",
+  },
+  {
+    name: "Donations",
+    icon: <VolunteerActivismOutlinedIcon />,
+    activeIcon: <VolunteerActivismIcon />,
+    path: "/Donations",
+  },
+  {
+    name: "Settings",
+    icon: <SettingsOutlinedIcon />,
+    activeIcon: <SettingsIcon />,
+    path: "/Settings",
+  },
+];
 const SideBar = () => {
-
   return (
-    <>
-    
-    <>
-   
-      <ul className="d-none d-md-block ">
-        <NavLink to="/" className="d-flex ps-3 ps-3">
-          {({ isActive }) => (
-            <>
-              {isActive ? <SpaceDashboardIcon /> : <SpaceDashboardOutlinedIcon />}
-              <span>&nbsp;Dashboard</span>
-            </>
-          )}
-        </NavLink>
-
-        <NavLink to="report" className="d-flex ps-3">
-        {({ isActive }) => (
-            <>
-              {isActive ? <SummarizeIcon /> : <SummarizeOutlinedIcon />}
-              <span>&nbsp;Report</span>
-            </>
-          )}
-        </NavLink>
-        <NavLink to="/Accounts" className="d-flex ps-3">
-        {({ isActive }) => (
-            <>
-              {isActive ? <AccountBalanceWalletIcon /> : <AccountBalanceWalletOutlinedIcon />}
-              <span>&nbsp;Accounts</span>
-            </>
-          )}
-        </NavLink>
-        <NavLink to="/Members" className="d-flex ps-3">
-          {({ isActive }) => (
-            <>
-              {isActive ? <PeopleAltIcon /> : <PeopleOutlinedIcon />}
-              <span>&nbsp;Members</span>
-            </>
-          )}
-        </NavLink>
-        <NavLink to="/Transaction" className="d-flex ps-3">
-        {({ isActive }) => (
-            <>
-              {isActive ? <ReceiptLongIcon /> : <ReceiptLongOutlinedIcon />}
-              <span>&nbsp;Transaction</span>
-            </>
-          )}
-        </NavLink>
-        <NavLink to="/Sandha" className="d-flex ps-3">
-        {({ isActive }) => (
-            <>
-              {isActive ? <PaidIcon /> : <PaidOutlinedIcon />}
-              <span>&nbsp;Sandha</span>
-            </>
-          )}
-        </NavLink>
-        <NavLink to="/Donations" className="d-flex ps-3">
-        {({ isActive }) => (
-            <>
-              {isActive ? <VolunteerActivismIcon /> : <VolunteerActivismOutlinedIcon />}
-              <span>&nbsp;Donations</span>
-            </>
-          )}
-        </NavLink>
-        <NavLink to="/Settings" className="d-flex ps-3">
-        {({ isActive }) => (
-            <>
-              {isActive ? <SettingsIcon /> : <SettingsOutlinedIcon />}
-              <span>&nbsp;Settings</span>
-            </>
-          )}
-        </NavLink>
-      </ul>
-    </>
-    </>
+    <ul className="d-none d-md-block ">
+      {item.map((navitem, key) => {
+        return (
+          <NavLink to={navitem.path} className="d-flex ps-3 ps-3" key={key}>
+            {({ isActive }) => (
+              <>
+                {isActive ? navitem.activeIcon : navitem.icon}
+                <span>&nbsp;{navitem.name}</span>
+              </>
+            )}
+          </NavLink>
+        );
+      })}
+    </ul>
   );
 };
 
